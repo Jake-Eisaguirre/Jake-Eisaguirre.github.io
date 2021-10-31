9e5b3b06513dc2ed60da689e2124a754210beb5a
Jake Eisaguirre <88209553+Jake-Eisaguirre@users.noreply.github.com>
2021-10-31 16:19:30 -0700

    blog

---
+++ _posts/2021-10-31-combining-data/wind_data.R
@@ -0,0 +1,7 @@
+library(here)
+
+noaa_east_buoy_data <- read.csv(here("data", "noaa_wind_data.csv"))
+
+noaa_santa_monica_buoy_data <- read.csv(here("data", "noaa_santa_monica_data.csv"))
+
+noaa_west_buoy_data <- read.csv(here("data", "noaa_west_data.csv"))
